a3b90d65448f2b0a3720cda4e52aaf15b290cee9
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-11-23 18:36:10 +0200

    removed header from AuthNavigator

--- src/components/App/AppNavigator/AuthNavigator/index.tsx
+++ src/components/App/AppNavigator/AuthNavigator/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
+// import React from 'react';
 import { createStackNavigator } from 'react-navigation';
 
-import navService from '../../../../shared/services/nav.service';
-import SigninScreen from './SigninScreen';
-import SignupScreen from './SignupScreen';
+// import navService from '../../../../shared/services/nav.service';
+import LoginScreen from './LoginScreen';
+// import RegistrationScreen from './RegistrationScreen';
 
 export default createStackNavigator({
   LoginScreen: {
-    screen: SignupScreen,
-    navigationOptions: navService.navigationOptions('Login'),
+    screen: LoginScreen,
+    navigationOptions: {
+      header: null,
+    },
   },
-  RegistrationScreen: {
-    screen: SigninScreen,
-    navigationOptions: navService.navigationOptions('Registration'),
-  },
+  // RegistrationScreen: {
+  //   screen: RegistrationScreen,
+  //   navigationOptions: navService.navigationOptions('Registration'),
+  // },
 }, {
   initialRouteName: 'LoginScreen',
 });
