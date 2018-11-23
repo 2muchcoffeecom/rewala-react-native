2c659fe9b5904f27684b4be1969cc12c7db23a0c
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-11-23 18:36:44 +0200

    added common app.style

---
+++ src/app.style.ts
@@ -0,0 +1,20 @@
+import { Dimensions, TextStyle } from 'react-native';
+
+export const whiteColor = '#FFF';
+export const greyColor = '#9D9D9D';
+export const mainColor = '#708EFF';
+export const blackColor = '#1C1C1C';
+export const shadowColor = '#CCCBCB';
+
+export const fontFamily = 'Lato';
+
+export const headerTitleStyle: TextStyle = {
+  fontFamily,
+  fontWeight: '400',
+  fontSize: 17,
+  color: blackColor,
+  alignSelf: 'center',
+};
+
+export const fullWidth = Dimensions.get('window').width - 20;
+export const fullHeight = Dimensions.get('window').height - 25;
