33de72e8c24dc344542e84e23e7ec4c132d59713
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-11-27 17:19:34 +0200

    added authMiddleware

--- src/shared/middlewares/link.middleware.ts
+++ src/shared/middlewares/link.middleware.ts
@@ -1,10 +1,28 @@
 import { onError } from 'apollo-link-error';
+import { setContext } from 'apollo-link-context';
 import { ApolloLink } from 'apollo-link';
 import { createUploadLink } from 'apollo-upload-client';
+import { map } from 'rxjs/operators';
+import authService from '../../shared/services/auth.service';
 
 import { apiEndpoint } from '../constants/apiEndpoint';
 
 const link = createUploadLink({uri: `${apiEndpoint}/graphql`});
+
+const authMiddleware = setContext(() => {
+  const token$ = authService.getToken().pipe(
+    map(token => {
+      return {
+        headers: {
+          authorization: `Bearer ${token}` || null,
+        },
+      };
+    }),
+  );
+  return new Promise((next, error) => {
+    token$.subscribe({next, error});
+  });
+});
 
 const errorMiddleware = onError(({graphQLErrors, networkError}) => {
   if (graphQLErrors) {
@@ -21,6 +39,7 @@ const errorMiddleware = onError(({graphQLErrors, networkError}) => {
 });
 
 export default ApolloLink.from([
+  authMiddleware,
   errorMiddleware,
   link,
 ]);
