9c9676922ad1a376847f34843a226b25bf2d5f5c
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-11-27 17:15:48 +0200

    added setToken epic

--- src/redux/auth/epics/index.ts
+++ src/redux/auth/epics/index.ts
@@ -1,9 +1,10 @@
 import { Action } from 'redux';
 import { Observable } from 'rxjs';
 import { ofType } from 'redux-observable';
-import { map } from 'rxjs/operators';
+import { ignoreElements, map, switchMap } from 'rxjs/operators';
 import * as fromActions from '../AC';
 import { authRequestAC } from '../../request/nested-states/auth/AC';
+import authService from '../../../shared/services/auth.service';
 
 const loginEpic = (action$: Observable<Action>) => action$.pipe(
   ofType<fromActions.Actions>(
@@ -23,7 +24,22 @@ const registrationEpic = (action$: Observable<Action>) => action$.pipe(
   }),
 );
 
+const setTokenEpic = (action$: Observable<Action>) => action$.pipe(
+  ofType<ReturnType<typeof authRequestAC.login.Actions.loginSuccess> |
+    ReturnType<typeof authRequestAC.registration.Actions.registrationSuccess>>(
+    authRequestAC.login.ActionTypes.LOGIN_SUCCESS,
+    authRequestAC.registration.ActionTypes.REGISTRATION_SUCCESS,
+  ),
+  switchMap((action) => {
+    const user = action.payload.data;
+    return authService.setToken(user.authToken);
+  }),
+  ignoreElements(),
+  )
+;
+
 export const authEpics = [
   loginEpic,
   registrationEpic,
+  setTokenEpic,
 ];
