74075c4c4e29f3f2fd311226482f9459db97b910
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-11-30 12:46:21 +0200

    added resetPasswordToastEpic

--- src/redux/toast/epics/index.ts
+++ src/redux/toast/epics/index.ts
@@ -1,33 +1,21 @@
-// import { Action } from 'redux';
-// import { Observable } from 'rxjs';
-// import { ofType } from 'redux-observable';
-// import { map } from 'rxjs/operators';
-// import * as fromActions from '../AC';
-// import { authRequestAC } from '../../request/nested-states/auth/AC';
-//
-// const showErrorRequestToastEpic = (action$: Observable<Action>) => action$.pipe(
-//   ofType<ReturnType<typeof authRequestAC.login.Actions.loginFail> |
-//     ReturnType<typeof authRequestAC.registration.Actions.registrationFail>>(
-//     authRequestAC.login.ActionTypes.LOGIN_FAIL,
-//     authRequestAC.registration.ActionTypes.REGISTRATION_FAIL,
-//   ),
-//   map((action) => {
-//     const fields = action.payload.errors.fields;
-//     const message = action.payload.errors.message;
-//     let error;
-//
-//     if (fields && fields.email && fields.email.unique) {
-//       error = fields.email.unique;
-//     } else if (message) {
-//       error = message;
-//     } else {
-//       error = '';
-//     }
-//
-//     return fromActions.Actions.showToast(error);
-//   }),
-// );
-//
-// export const toastEpics = [
-//   showErrorRequestToastEpic,
-// ];
+import { Action } from 'redux';
+import { Observable } from 'rxjs';
+import { ofType } from 'redux-observable';
+import { map } from 'rxjs/operators';
+import * as fromActions from '../AC';
+import { authRequestAC } from '../../request/nested-states/auth/AC';
+
+const showResetPasswordToastEpic = (action$: Observable<Action>) => action$.pipe(
+  ofType<ReturnType<typeof authRequestAC.resetPassword.Actions.resetPasswordSuccess>>(
+    authRequestAC.resetPassword.ActionTypes.RESET_PASSWORD_SUCCESS,
+  ),
+  map(() => {
+      return fromActions.Actions.showToast(
+        'The verification code was sent to your account email address.',
+      );
+  }),
+);
+
+export const toastEpics = [
+  showResetPasswordToastEpic,
+];
