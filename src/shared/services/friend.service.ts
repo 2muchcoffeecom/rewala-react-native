90de2711f6e25dbaa21595c91312d826f7e7cdff
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-04 17:15:36 +0200

    added friend.service

---
+++ src/shared/services/friend.service.ts
@@ -0,0 +1,56 @@
+import { from, Observable, ObservableInput } from 'rxjs';
+import gql from 'graphql-tag';
+import { execute } from 'apollo-link';
+
+import link from '../middlewares/link.middleware';
+import { followRequest } from '../templates/followRequest.template';
+import { FollowRequestStatus } from '../models/followRequest.model';
+
+interface CreateFollowRequestInput {
+  toUserId: string;
+}
+
+export interface UpdateFollowRequestInput {
+  _id: string;
+  status: FollowRequestStatus;
+}
+
+interface IFriendService {
+  createFollowRequest(input: CreateFollowRequestInput): Observable<any>;
+  updateFollowRequest(input: UpdateFollowRequestInput): Observable<any>;
+}
+
+class FriendService implements IFriendService {
+
+  createFollowRequest(input: CreateFollowRequestInput) {
+    const operation = {
+      query: gql`
+        mutation createFollowRequest($input: CreateFollowRequestInput) {
+          createFollowRequest(input: $input) ${followRequest}
+        }
+      `,
+      variables: {
+        input,
+      },
+    };
+
+    return from(execute(link, operation) as ObservableInput<any>);
+  }
+
+  updateFollowRequest(input: UpdateFollowRequestInput) {
+    const operation = {
+      query: gql`
+        mutation updateFollowRequest($input: UpdateFollowRequestInput) {
+          updateFollowRequest(input: $input) ${followRequest}
+        }
+      `,
+      variables: {
+        input,
+      },
+    };
+
+    return from(execute(link, operation) as ObservableInput<any>);
+  }
+}
+
+export default new FriendService();
