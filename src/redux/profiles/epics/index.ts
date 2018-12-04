06c2ffb95253276d5f505e2a0a7f20d18433dbe9
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-04 13:18:31 +0200

    added saving user after login, registration, newPassword

--- src/redux/profiles/epics/index.ts
+++ src/redux/profiles/epics/index.ts
@@ -3,15 +3,23 @@ import { Observable } from 'rxjs';
 import { ofType } from 'redux-observable';
 import { map } from 'rxjs/operators';
 import * as fromActions from '../AC';
-import { contactsRequestAC } from '../../request/nested-states/contacts/AC';
+import { contactsRequestAC, authRequestAC } from '../../request/AC';
 import { ProfileModel } from '../../../shared/models/profile.model';
 
 const setProfilesDataEpic = (action$: Observable<Action>) => action$.pipe(
-  ofType<ReturnType<typeof contactsRequestAC.sendContacts.Actions.contactsSendSuccess>>(
+  ofType<ReturnType<typeof contactsRequestAC.sendContacts.Actions.contactsSendSuccess> |
+    ReturnType<typeof authRequestAC.login.Actions.loginSuccess> |
+    ReturnType<typeof authRequestAC.registration.Actions.registrationSuccess> |
+    ReturnType<typeof authRequestAC.newPassword.Actions.newPasswordSuccess>>(
     contactsRequestAC.sendContacts.ActionTypes.CONTACTS_SEND_SUCCESS,
+    authRequestAC.login.ActionTypes.LOGIN_SUCCESS,
+    authRequestAC.registration.ActionTypes.REGISTRATION_SUCCESS,
+    authRequestAC.newPassword.ActionTypes.NEW_PASSWORD_SUCCESS,
   ),
   map((action) => {
-    const profiles = action.payload.data.map<ProfileModel>((user) => user.profile);
+    const profiles = Array.isArray(action.payload.data) ?
+      action.payload.data.map<ProfileModel>((user) => user.profile) :
+      [action.payload.data.profile];
 
     return fromActions.Actions.setProfilesData(profiles);
   }),
--- src/redux/users/epics/index.ts
+++ src/redux/users/epics/index.ts
@@ -3,14 +3,22 @@ import { Observable } from 'rxjs';
 import { ofType } from 'redux-observable';
 import { map } from 'rxjs/operators';
 import * as fromActions from '../AC';
-import { contactsRequestAC } from '../../request/nested-states/contacts/AC';
+import { contactsRequestAC, authRequestAC } from '../../request/AC';
 
 const setUsersDataEpic = (action$: Observable<Action>) => action$.pipe(
-  ofType<ReturnType<typeof contactsRequestAC.sendContacts.Actions.contactsSendSuccess>>(
+  ofType<ReturnType<typeof contactsRequestAC.sendContacts.Actions.contactsSendSuccess> |
+    ReturnType<typeof authRequestAC.login.Actions.loginSuccess> |
+    ReturnType<typeof authRequestAC.registration.Actions.registrationSuccess> |
+    ReturnType<typeof authRequestAC.newPassword.Actions.newPasswordSuccess>>(
     contactsRequestAC.sendContacts.ActionTypes.CONTACTS_SEND_SUCCESS,
+    authRequestAC.login.ActionTypes.LOGIN_SUCCESS,
+    authRequestAC.registration.ActionTypes.REGISTRATION_SUCCESS,
+    authRequestAC.newPassword.ActionTypes.NEW_PASSWORD_SUCCESS,
   ),
   map((action) => {
-    const users = action.payload.data;
+    const users = Array.isArray(action.payload.data) ?
+      action.payload.data :
+      [action.payload.data];
 
     return fromActions.Actions.setUsersData(users);
   }),
