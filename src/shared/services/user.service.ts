136a7efbb4ad8819fd82ec4b2a8697ce9c271fa4
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-11 18:49:44 +0200

    added updateUser request

--- src/shared/services/user.service.ts
+++ src/shared/services/user.service.ts
@@ -1,12 +1,27 @@
 import { from, Observable, ObservableInput } from 'rxjs';
 import gql from 'graphql-tag';
 import { execute } from 'apollo-link';
+import { ReactNativeFile } from 'apollo-upload-client';
 
 import link from '../middlewares/link.middleware';
 import { user } from '../templates/user.template';
 
+interface ProfileInput {
+  fullName: string;
+  phone?: string;
+  countryCode?: string;
+  notifications: boolean;
+  avatar: InstanceType<typeof ReactNativeFile>;
+}
+
+export interface UpdateUserInput {
+  email: string;
+  profileInput: ProfileInput;
+}
+
 interface IUserService {
   getMe(): Observable<any>;
+  updateMe(input: UpdateUserInput): Observable<any>;
 }
 
 class UserService implements IUserService {
@@ -18,6 +33,20 @@ class UserService implements IUserService {
           me ${user}
         }
       `,
+    };
+    return from(execute(link, operation) as ObservableInput<any>);
+  }
+
+  updateMe(input: UpdateUserInput) {
+    const operation = {
+      query: gql`
+        mutation updateMe($input: UpdateUserInput) {
+          updateMe(input: $input) ${user}
+        }
+      `,
+      variables: {
+        input,
+      },
     };
     return from(execute(link, operation) as ObservableInput<any>);
   }
