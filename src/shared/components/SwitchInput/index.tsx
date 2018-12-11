1539e6ebddb38eda0caf6c51ed185dad129d57a2
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-11 18:48:59 +0200

    added SwitchInput component

---
+++ src/shared/components/SwitchInput/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Switch, SwitchProps } from 'react-native';
+import { WrappedFieldProps } from 'redux-form';
+import style from './style';
+import { mainColor } from '../../../app.style';
+
+type Props = SwitchProps & WrappedFieldProps;
+
+export const SwitchInput: React.FunctionComponent<Props> = (props: Props) => {
+  const {input: {onChange, ...restInput}, meta, ...restProps} = props;
+
+  return (
+    <Switch
+      onValueChange={onChange}
+      {...restInput}
+      thumbColor={mainColor}
+      trackColor={{false: '#D8D8D8', true: '#D8D8D8'}}
+      {...restProps}
+      style={style.switch}
+    />
+  );
+};
+
+export default SwitchInput;
---
+++ src/shared/components/SwitchInput/style.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+const style = StyleSheet.create({
+  switch: {
+    width: 34,
+    height: 20,
+  },
+});
+
+export default style;
