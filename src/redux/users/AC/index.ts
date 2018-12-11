9ce2fcb395632ea68da9bcfb69c84edff8a10699
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-11 18:53:07 +0200

    added update authorized user action

--- src/redux/users/AC/index.ts
+++ src/redux/users/AC/index.ts
@@ -1,9 +1,12 @@
 import { createAction, ActionsUnion } from '../../../shared/helpers/createAction';
 import { UserResponse } from '../../../shared/models/user.model';
+import { UpdateUserInput } from '../../../shared/services/user.service';
+import { Reject, Resolve } from "../../request/states";
 
 export enum ActionTypes {
   SET_USERS_DATA = 'SET_USERS_DATA',
   GET_AUTHORIZED_USER = 'GET_AUTHORIZED_USER',
+  UPDATE_AUTHORIZED_USER = 'UPDATE_AUTHORIZED_USER',
 }
 
 export const Actions = {
@@ -12,6 +15,9 @@ export const Actions = {
   },
   getAuthorizedUser: () => {
     return createAction(ActionTypes.GET_AUTHORIZED_USER);
+  },
+  updateAuthorizedUser: (data: UpdateUserInput, resolve?: Resolve<UserResponse>, reject?: Reject) => {
+    return createAction(ActionTypes.GET_AUTHORIZED_USER, {data, resolve, reject});
   },
 };
 
