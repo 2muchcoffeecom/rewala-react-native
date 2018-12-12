025174a31f21db51d76384b22434d2c166397481
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-12 17:05:08 +0200

    added errors handling and form submition

--- src/shared/components/ChangePasswordModal/index.tsx
+++ src/shared/components/ChangePasswordModal/index.tsx
@@ -6,16 +6,23 @@ import { Modal, TouchableOpacity, View, Text, Image } from 'react-native';
 import Input from '../Input';
 import RegularButton from '../RegularButton';
 
-import {
-  NewPasswordFormData as ChangePasswordFormData,
-} from '../../../components/App/AppNavigator/AuthNavigator/NewPasswordScreen';
 import { Dispatch } from 'redux';
-import { Actions as authActions } from '../../../redux/auth/AC';
 import { RequestError } from '../../../redux/request/states';
+import { UserResponse } from '../../models/user.model';
+import { ChangePasswordInput } from '../../services/auth.service';
+
+import { Actions as authActions } from '../../../redux/auth/AC';
+import { Actions as toastActions } from '../../../redux/toast/AC';
 
 import required from '../../validators/required';
 import { confirmPassword, passwordRegistration } from '../../validators/password';
 import { getSubmissionError } from '../../validators/getSubmissionError';
+
+export interface ChangePasswordFormData {
+  oldPassword: string;
+  newPassword: string;
+  passwordConfirm: string;
+}
 
 interface OwnProps {
   isVisible: boolean;
@@ -25,21 +32,27 @@ interface OwnProps {
 type Props = OwnProps & InjectedFormProps<ChangePasswordFormData, OwnProps>;
 
 const ChangePasswordModal: React.FunctionComponent<Props> = (props) => {
-  const submitChangePassword = (values: ChangePasswordFormData, dispatch: Dispatch<authActions>) => {
-    // const resetPasswordConfirmInput: ResetPasswordConfirmInput = {
-    //   password: values.password,
-    //   resetPasswordCode: formValuesResetPasswordCode.resetPasswordCode,
-    // };
-    //
-    // return new Promise<UserResponse>((resolve, reject) => {
-    //   dispatch(authActions.submitNewPassword(resetPasswordConfirmInput, resolve, reject));
-    // })
-    //   .catch((error: RequestError) => {
-    //     throw new SubmissionError<ChangePasswordFormData>({
-    //       password: getSubmissionError(error, 'password'),
-    //       _error: error.message ? error.message : undefined,
-    //     });
-    //   });
+  const submitChangePassword = (
+    values: ChangePasswordFormData,
+    dispatch: Dispatch<authActions | toastActions>,
+  ) => {
+    const changePasswordInput: ChangePasswordInput = {
+      oldPassword: values.oldPassword,
+      newPassword: values.newPassword,
+    };
+
+    return new Promise<UserResponse>((resolve, reject) => {
+      dispatch(authActions.submitChangePassword(changePasswordInput, resolve, reject));
+    })
+      .catch((error: RequestError) => {
+        error.message && dispatch(toastActions.showToast(error.message));
+
+        throw new SubmissionError<ChangePasswordFormData>({
+          oldPassword: getSubmissionError(error, 'oldPassword'),
+          newPassword: getSubmissionError(error, 'newPassword'),
+          _error: error.message ? error.message : undefined,
+        });
+      });
   };
 
   const {isVisible, handleSubmit, toggleVisibility} = props;
