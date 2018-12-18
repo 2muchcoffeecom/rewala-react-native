15fa958105df457dcac18cbbc327d2ddad33b697
Dmitriy Melnichenko <tairezzzz@gmail.com>
2018-12-18 15:24:38 +0200

    added new forms to state interface

--- src/redux/form/states/index.ts
+++ src/redux/form/states/index.ts
@@ -6,4 +6,6 @@ export interface FormsState {
   forgotPassword: FormsState;
   newPassword: FormsState;
   resetPasswordCode: FormsState;
+  ProfileSettings: FormsState;
+  changePassword: FormsState;
 }
