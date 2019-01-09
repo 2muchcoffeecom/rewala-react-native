fcafa743f1ac9c5de3086a2abc2b6ee59e58b56f
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-01-09 18:20:17 +0200

    Rewrite to class component
    Add state for control modal visibility

--- src/shared/components/RewalBackground/index.tsx
+++ src/shared/components/RewalBackground/index.tsx
@@ -10,25 +10,35 @@ import LinearGradient from 'react-native-linear-gradient';
 interface OwnProps {
   title: string;
   titleColor: string;
-  isVisible: boolean;
-  toggleVisibility(): void;
   backgroundPath?: string;
   backgroundRatio?: number;
 }
 
 type Props = OwnProps;
 
-const RewalBackground: React.FunctionComponent<Props> = (props) => {
-  const {isVisible, title, titleColor, backgroundPath, toggleVisibility} = props;
+interface State {
+  isVisibleModal: boolean;
+}
 
-  const onCloseModal = () => {
-    toggleVisibility();
-  };
+class RewalBackground extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      isVisibleModal: false,
+    };
+  }
 
-  // const backgroundHeight = Dimensions.get('window').width / props.backgroundRatio;
-  const backgroundUri = `${apiEndpoint}${backgroundPath}`;
+  toggleModalVisibility = () => {
+    this.setState((state) => ({
+      isVisibleModal: !state.isVisibleModal,
+    }));
+  }
 
-  const getGradient = (): JSX.Element => (
+  onCloseModal = () => {
+    this.toggleModalVisibility();
+  }
+
+  getGradient = (): JSX.Element => (
     <LinearGradient
       colors={linearGradientColors}
       useAngle={true}
@@ -36,58 +46,63 @@ const RewalBackground: React.FunctionComponent<Props> = (props) => {
       style={style.gradient}
     >
       <Text
-        style={[style.textTitle, {color: titleColor}]}
+        style={[style.textTitle, {color: this.props.titleColor}]}
       >
-        {title}
+        {this.props.title}
       </Text>
     </LinearGradient>
-  );
+  )
 
-  const getBackgroundImage = (): JSX.Element => (
-    <React.Fragment>
-      <TouchableOpacity
-        onPress={toggleVisibility}
-        style={style.gradient}
-      >
-        <Text
-          style={[style.textTitle, {color: titleColor}]}
+  getBackgroundImage = (): JSX.Element => {
+    const backgroundUri = `${apiEndpoint}${this.props.backgroundPath}`;
+    const {title, titleColor} = this.props;
+
+    return (
+      <React.Fragment>
+        <TouchableOpacity
+          onPress={this.toggleModalVisibility}
+          style={style.gradient}
         >
-          {title}
-        </Text>
-        <Image
-          source={{uri: backgroundUri}}
-          resizeMode='cover'
-          style={style.backgroundImage}
-        />
-      </TouchableOpacity>
-      <Modal
-        onBackdropPress={onCloseModal}
-        backdropColor={blackModalColor}
-        backdropOpacity={0.74}
-        onBackButtonPress={onCloseModal}
-        isVisible={isVisible}
-        deviceHeight={Dimensions.get('screen').height}
-      >
-        <View style={style.modalRoot}>
-          <View style={style.modalContainer}>
-            <Image
-              source={{uri: backgroundUri}}
-              resizeMode='contain'
-              style={
-                [style.backgroundFullScreen/*, {height: avatarHeight}*/]
-              }
-            />
+          <Text
+            style={[style.textTitle, {color: titleColor}]}
+          >
+            {title}
+          </Text>
+          <Image
+            source={{uri: backgroundUri}}
+            resizeMode='cover'
+            style={style.backgroundImage}
+          />
+        </TouchableOpacity>
+        <Modal
+          onBackdropPress={this.onCloseModal}
+          backdropColor={blackModalColor}
+          backdropOpacity={0.74}
+          onBackButtonPress={this.onCloseModal}
+          isVisible={this.state.isVisibleModal}
+          deviceHeight={Dimensions.get('screen').height}
+        >
+          <View style={style.modalRoot}>
+            <View style={style.modalContainer}>
+              <Image
+                source={{uri: backgroundUri}}
+                resizeMode='contain'
+                style={style.backgroundFullScreen}
+              />
+            </View>
           </View>
-        </View>
-      </Modal>
-    </React.Fragment>
-  );
+        </Modal>
+      </React.Fragment>
+    );
+  }
 
-  return (
-    <View style={style.bgImageContainer}>
-      {!backgroundPath ? getGradient() : getBackgroundImage()}
-    </View>
-  );
-};
+  render() {
+    return (
+      <View style={style.bgImageContainer}>
+        {!this.props.backgroundPath ? this.getGradient() : this.getBackgroundImage()}
+      </View>
+    );
+  }
+}
 
-export default React.memo(RewalBackground);
+export default RewalBackground;
